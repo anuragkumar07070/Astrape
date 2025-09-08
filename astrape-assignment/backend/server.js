9465761be9996ab8497867c021c4a7e553649cf7
anuragkumar07070 <anuragkumar07070@gmail.com>
2025-09-08 20:31:56 +0530

    add root route to confirm server is running

--- astrape-assignment/backend/server.js
+++ astrape-assignment/backend/server.js
@@ -44,7 +44,12 @@ app.use((req, res) => {
   });
 });
 
+app.get('/',(req,res)=>{
+  res.send("server is running...");
+})
+
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
